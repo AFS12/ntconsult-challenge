8e6a2fa87ecd499695150d24f87a1830ea7fef49
AFS12 <arthurmbayres@gmail.com>
2024-06-21 02:03:21 -0300

    adição de logo e nome do sistema e base do header

--- src/views/layouts/Default.vue
+++ src/views/layouts/Default.vue
@@ -1,6 +1,36 @@
 <template>
-  <h1>Base</h1>
-  <router-view/>
+  <v-toolbar>
+    <v-toolbar-title class="header">
+      <div
+        class="logo"
+        @click="$router.push({ name: 'home' })"
+      >
+        <v-img 
+          class="logo-img"
+          width="50"
+          height="50"
+          src="/img/systemLogo.svg"
+          alt="Check-In Point Logo" 
+        />
+        <h1 class="system-name">
+          Check-In Point
+        </h1>
+      </div>
+    </v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>mdi-export</v-icon>
+    </v-btn>
+  </v-toolbar>
+  <v-container 
+    fluid
+    class="main-content"
+  >
+    <h1>Base</h1>
+    <router-view/>
+  </v-container>
 </template>
 
 <script lang="ts">
