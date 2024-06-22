4eecef0c4cdc3129ec57f72a388f1679e622ec07
AFS12 <arthurmbayres@gmail.com>
2024-06-22 00:24:06 -0300

    Adaptando teste basico para o ci

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,11 +1,7 @@
 import { shallowMount } from '@vue/test-utils'
 
-// describe('HelloWorld.vue', () => {
-//   it('renders props.msg when passed', () => {
-//     const msg = 'new message'
-//     const wrapper = shallowMount(HelloWorld, {
-//       props: { msg }
-//     })
-//     expect(wrapper.text()).toMatch(msg)
-//   })
-// })
+describe('Example test', () => {
+  it('runs a basic test', () => {
+    expect(true).toBe(true)
+  })
+})
